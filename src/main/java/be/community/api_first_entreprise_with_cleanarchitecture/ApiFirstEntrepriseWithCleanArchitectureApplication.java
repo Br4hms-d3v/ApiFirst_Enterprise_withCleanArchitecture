0e0ae6e8d450dd51b3eb2812d97813617ac01023
Brahms <bramso.brahms@gmail.com>
2026-04-08 21:44:12 +0200

    Add Spotless

--- src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/ApiFirstEntrepriseWithCleanArchitectureApplication.java
+++ src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/ApiFirstEntrepriseWithCleanArchitectureApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiFirstEntrepriseWithCleanArchitectureApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiFirstEntrepriseWithCleanArchitectureApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(ApiFirstEntrepriseWithCleanArchitectureApplication.class, args);
+  }
 }
