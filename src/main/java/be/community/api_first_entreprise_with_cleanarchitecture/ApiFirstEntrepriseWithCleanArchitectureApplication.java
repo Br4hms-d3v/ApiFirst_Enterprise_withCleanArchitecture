ec2190684bbceb342dce741c3411db469ddf7be8
Brahms <bramso.brahms@gmail.com>
2026-04-08 21:05:58 +0200

    Refactoring the app => api_employee

---
+++ src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/ApiFirstEntrepriseWithCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package be.community.api_first_entreprise_with_cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstEntrepriseWithCleanArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFirstEntrepriseWithCleanArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/community/api_first_entreprise_with_cleanarchitecture/ApiFirstEntrepriseWithCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package be.community.api_first_entreprise_with_cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstEntrepriseWithCleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
