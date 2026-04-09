94215629027b4e64028a25284829d19aa1ae3d5e
Brahms <bramso.brahms@gmail.com>
2026-04-09 23:58:02 +0200

    Indentation adapted

--- src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/address/dto/AddressDto.java
+++ src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/address/dto/AddressDto.java
@@ -2,14 +2,14 @@ package be.community.api_first_entreprise_with_cleanarchitecture.core.applicatio
 
 /** This class is a data transfer object for address. */
 public record AddressDto(
-        // The id of the address.
-        Long id,
+    // The id of the address.
+    Long id,
 
-        // The street name.
-        String street,
+    // The street name.
+    String street,
 
-        // The postcode.
-        Integer zipCode,
+    // The postcode.
+    Integer zipCode,
 
-        // The city name.
-        String city) {}
+    // The city name.
+    String city) {}
