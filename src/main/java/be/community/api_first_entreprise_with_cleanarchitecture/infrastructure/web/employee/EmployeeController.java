ad4f062acda088a432ca5296641f29faad0302b3
Brahms <bramso.brahms@gmail.com>
2026-04-13 00:57:37 +0200

    Create Ressource employee

--- src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/infrastructure/web/employee/EmployeeController.java
+++
@@ -1,3 +0,0 @@
-package be.community.api_first_entreprise_with_cleanarchitecture.infrastructure.web.employee;
-
-public class EmployeeController {}
---
+++ src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/infrastructure/web/employee/EmployeeResource.java
@@ -0,0 +1,7 @@
+package be.community.api_first_entreprise_with_cleanarchitecture.infrastructure.web.employee;
+
+import org.openapitools.api.EmployeeApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeResource implements EmployeeApi {}
