4a0c2b89943e88d9339c509841cbb406cf7fafdf
Brahms <bramso.brahms@gmail.com>
2026-04-13 00:54:06 +0200

    Update database H2

--- src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/employee/command/CreateEmployeeCommand.java
+++ src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/employee/command/CreateEmployeeCommand.java
@@ -1,3 +1,36 @@
 package be.community.api_first_entreprise_with_cleanarchitecture.core.application.employee.command;
 
-public record CreateEmployeeCommand() {}
+import be.community.api_first_entreprise_with_cleanarchitecture.core.application.address.dto.AddressDto;
+
+/**
+ * This command is used to create an employee
+ *
+ * @param id the identifier
+ * @param name the name of employee
+ * @param firstName the first name of employee
+ * @param email the email of employee
+ * @param service the service where work the employee
+ * @param floor the level floor where the employee work
+ * @param address the address where he lives
+ */
+public record CreateEmployeeCommand(
+    // The id of the employee.
+    Long id,
+
+    // The last name.
+    String name,
+
+    // The first name.
+    String firstName,
+
+    // The email.
+    String email,
+
+    // The service (department).
+    String service,
+
+    // The floor number.
+    Integer floor,
+
+    // The address of the employee.
+    AddressDto address) {}
