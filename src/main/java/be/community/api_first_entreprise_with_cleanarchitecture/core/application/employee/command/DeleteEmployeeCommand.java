c8118909f505039b5b54afaabde334f282354392
Brahms <bramso.brahms@gmail.com>
2026-04-13 00:54:33 +0200

    Delete Employee

--- src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/employee/command/DeleteEmployeeCommand.java
+++ src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/employee/command/DeleteEmployeeCommand.java
@@ -1,3 +1,10 @@
 package be.community.api_first_entreprise_with_cleanarchitecture.core.application.employee.command;
 
-public record DeleteEmployeeCommand() {}
+/**
+ * This command is used to delete an employee
+ *
+ * @param id
+ */
+public record DeleteEmployeeCommand(
+    // The id of the employee.
+    Long id) {}
