c53ca009534ea7e2524d1bc24a80100d3efa3252
Brahms <bramso.brahms@gmail.com>
2026-04-13 00:58:14 +0200

    Search employee

--- src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/employee/query/EmployeeSearchQuery.java
+++ src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/employee/query/EmployeeSearchQuery.java
@@ -1,3 +1,26 @@
 package be.community.api_first_entreprise_with_cleanarchitecture.core.application.employee.query;
 
-public record EmployeeSearchQuery() {}
+/**
+ * This query is used to search employees.
+ *
+ * @param name the name of employee
+ * @param firstName the first name of employee
+ * @param service the service where the employee works
+ * @param page the number of pages
+ * @param size the size of number of page
+ */
+public record EmployeeSearchQuery(
+    // The name
+    String name,
+
+    // The first name
+    String firstName,
+
+    // The service where he/she works
+    String service,
+
+    // Te number of page
+    int page,
+
+    // the size of page
+    int size) {}
