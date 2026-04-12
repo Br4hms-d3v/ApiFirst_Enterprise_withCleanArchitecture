621811404084541f1f56dd7136a1542b3ec958a5
Brahms <bramso.brahms@gmail.com>
2026-04-13 00:57:52 +0200

    List of employee

--- src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/employee/query/EmployeeListQuery.java
+++ src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/employee/query/EmployeeListQuery.java
@@ -1,3 +1,14 @@
 package be.community.api_first_entreprise_with_cleanarchitecture.core.application.employee.query;
 
-public record EmployeeListQuery() {}
+/**
+ * This query is used to get a list of employees.
+ *
+ * @param page the number of page
+ * @param size the size of page
+ */
+public record EmployeeListQuery(
+    // The page number
+    int page,
+
+    // The page size
+    int size) {}
