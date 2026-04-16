257d78417fb3313a1fbe14f36d0321da6f75a97b
Brahms <bramso.brahms@gmail.com>
2026-04-16 19:19:04 +0200

    Update: add Result.Unit

--- src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/common/Result.java
+++ src/main/java/be/community/api_first_entreprise_with_cleanarchitecture/core/application/common/Result.java
@@ -19,24 +19,65 @@ public class Result<E, T> {
     this.success = success;
   }
 
-  // Create a succes result.
+  // Success with value
   public static <E, T> Result<E, T> success(T value) {
     return new Result<>(null, value, true);
   }
 
-  // Create a failure result.
+  // Failure
   public static <E, T> Result<E, T> failure(E error) {
     return new Result<>(error, null, false);
   }
 
+  // Success without value (Unit)
+  public static <E, T> Result<E, Unit> success() {
+    return Result.success(Unit.INSTANCE);
+  }
+
   // Give only a failure or a succes
-  public <R> R fold(Function<E, R> onfailure, Function<T, R> onSuccess) {
+  public <R> R fold(Function<E, R> failure, Function<T, R> onSuccess) {
     if (success) {
       return onSuccess.apply(value);
     } else {
-      return onfailure.apply(error);
+      return failure.apply(error);
     }
   }
+
+  // Map
+  public <U> Result<E, U> map(Function<T, U> mapper) {
+    if (success) {
+      return Result.success(mapper.apply(value));
+    } else {
+      return Result.failure(error);
+    }
+  }
+
+  // FlatMap
+  public <U> Result<E, U> flatMap(Function<T, Result<E, U>> mapper) {
+    if (success) {
+      return mapper.apply(value);
+    } else {
+      return Result.failure(error);
+    }
+  }
+
+  // Unit
+  public static final class Unit {
+    public static final Unit INSTANCE = new Unit();
+
+    private Unit() {}
+
+    public static Unit unit() {
+      return INSTANCE;
+    }
+
+    @Override
+    public String toString() {
+      return "Unit";
+    }
+  }
+
+  /** Getters */
 
   // Check if result is success
   public boolean isSuccess() {
